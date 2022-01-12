05a744686eed0f68f3fb4b4cd91483f75d114b98
K.Hur <alsokyun@gmail.com>
2022-01-12 17:02:11 +0900

    ios initial

---
+++ www/js/page/Main.js
@@ -0,0 +1,30 @@
+$(document).on('pageinit', "#Main", function (e) {
+    e.preventDefault();
+    //$(document).off("pageinit", "#Main");
+});
+$(document).on('pagebeforeshow', "#Main",function () {
+      SetRcvIdx("");
+       isDone = "A";
+      //log("isDone, rcvIdx - " + isDone + ", " + GetRcvIdx());
+});
+
+$(document).on('pageshow', "#Main", function (e) {
+    e.preventDefault();
+
+    //startPrinterService();//프린터장비 연결 백그라운드 서비스
+});
+
+/*
+*/
+var startPrinterService = function(){
+    /**
+    프린터장비 연결 백그라운드 서비스
+    */
+    cordova.plugins.web_call.svcMethod([''],function(ret){
+        alert('svcMethod success - ' + ret);
+    },function(err){
+        //alert('svcMethod error - ' + err);
+    });
+    //    setTimeout(3000, function(){
+    //    });
+}
